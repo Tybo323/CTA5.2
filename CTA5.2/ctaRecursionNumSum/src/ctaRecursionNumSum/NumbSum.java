3e37de999e419778c02472e766278a29c3d38ced
Tybo323 <butoractyler@gmail.com>
2024-10-13 15:22:13 -0700

    Add files via upload

--- CTA5.2/ctaRecursionNumSum/src/ctaRecursionNumSum/NumbSum.java
+++ CTA5.2/ctaRecursionNumSum/src/ctaRecursionNumSum/NumbSum.java
@@ -6,16 +6,26 @@ import java.util.Scanner;
 
 public class NumbSum {
 	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[5];	
-        
-        System.out.println("Enter five numbers:");
+        try (Scanner scanner = new Scanner(System.in)) {
+			int[] numbers = new int[5];	
+			
+			System.out.println("Enter five numbers:");
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Number " + (i + 1) + ": ");
-            numbers[i] = scanner.nextInt();
-        }
-        int sum = calculateSum(numbers, 5);
-        System.out.println("The sum of the numbers is: " + sum);
+			for (int i = 0; i < 5; i++) {
+			    System.out.print("Number " + (i + 1) + ": ");
+			    numbers[i] = scanner.nextInt();
+			}
+			int sum = calculateSum(numbers, 5);
+			System.out.println("The sum of the numbers is: " + sum);
+		}
     }
+	
+      public static int calculateSum(int[] numbers, int n) {
+	        if (n <= 0) {
+	            return 0; // Base case: no numbers left to add
+	        } else {
+	            return numbers[n - 1] + calculateSum(numbers, n - 1); // Recursive case
+	        }
+	  }
 }
+
