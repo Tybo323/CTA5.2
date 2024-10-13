d3d5f1b41a4478556bb8c58b423202332d24f2fc
Tybo323 <butoractyler@gmail.com>
2024-10-13 15:16:39 -0700

    Add files via upload

--- CTA5.2/ctaRecursionNumSum/src/ctaRecursionNumSum/NumbSum.java
+++ CTA5.2/ctaRecursionNumSum/src/ctaRecursionNumSum/NumbSum.java
@@ -1,5 +1,21 @@
 package ctaRecursionNumSum;
 
-public class NumbSum {
+import java.util.Scanner;
 
+//This program allows user to enter five numbers, then calculates sum of these numbers with recursion.
+
+public class NumbSum {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];	
+        
+        System.out.println("Enter five numbers:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+        int sum = calculateSum(numbers, 5);
+        System.out.println("The sum of the numbers is: " + sum);
+    }
 }
